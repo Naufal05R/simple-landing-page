8141eb825b2930bde1d4170f424943b2906aa50e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-02-10 23:21:01 +0700

    feat(page): insert the benefits

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { benefits } from "@/constants/benefits";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -61,6 +62,42 @@ export default function Home() {
               width={720}
               height={720}
             />
+          ))}
+        </div>
+
+        <h3 className="text-center font-bold text-2xl lg:text-4xl">
+          Keuntungan Gabung Mitra
+        </h3>
+        <div className="flex flex-wrap justify-center">
+          {benefits.map(({ title, description, Icon }, i) => (
+            <div
+              key={i}
+              className="min-[450px]:basis-1/2 md:basis-1/3 flex flex-col items-center text-center min-[450px]:p-4 md:p-8"
+            >
+              <Icon size={50} className="text-red-500" />
+              <h4 className="text-2xl lg:text-4xl text-red-300 font-medium">
+                {title}
+              </h4>
+              <p>{description}</p>
+            </div>
+          ))}
+        </div>
+
+        <h3 className="text-center font-bold text-2xl lg:text-4xl">
+          Paket Mitra
+        </h3>
+        <div className="flex flex-wrap justify-center">
+          {benefits.map(({ title, description, Icon }, i) => (
+            <div
+              key={i}
+              className="min-[450px]:basis-1/2 md:basis-1/3 flex flex-col items-center text-center min-[450px]:p-4 md:p-8"
+            >
+              <Icon size={50} className="text-red-500" />
+              <h4 className="text-2xl lg:text-4xl text-red-300 font-medium">
+                {title}
+              </h4>
+              <p>{description}</p>
+            </div>
           ))}
         </div>
       </main>
