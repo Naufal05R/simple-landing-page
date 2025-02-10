460622dca0ca1942f694af4a662c708363017a64
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-02-10 23:20:49 +0700

    feat(constant): create a new benefits constant

---
+++ constants/benefits.ts
@@ -0,0 +1,67 @@
+import {
+  FaUsers,
+  FaBookReader,
+  FaDeezer,
+  FaMedal,
+  FaUserShield,
+  FaImages,
+  FaComments,
+  FaBuilding,
+  FaTasks,
+} from "react-icons/fa";
+
+export const benefits = [
+  {
+    title: "Komunitas",
+    description:
+      "Setiap Mitra akan memiliki Leader pada setiap kota/kabupaten.",
+    Icon: FaUsers,
+  },
+  {
+    title: "Ilmu",
+    description:
+      "Setiap satu minggu sekali akan mendapat pelatihan All mitra untuk peningkatan dalam berbisnis.",
+    Icon: FaBookReader,
+  },
+  {
+    title: "Laba",
+    description:
+      "Setiap pembelian produk mendapat keuntungan dalam bentuk diskon sesuai jenis kemitraan.",
+    Icon: FaDeezer,
+  },
+  {
+    title: "Reward",
+    description:
+      "Distributor Berkesempatan mendapat reward berupa Umroh, Liburan keluar negeri, Emas Antam, dan sebagainya.",
+    Icon: FaMedal,
+  },
+  {
+    title: "Leader",
+    description:
+      "Mendapat arahan langsung dari SR12 team pusat via zoom dan lokasi langsung.",
+    Icon: FaUserShield,
+  },
+  {
+    title: "Konten",
+    description:
+      "Mendapatkan kebutuhan konten posting untuk marketplace maupun sosial media.",
+    Icon: FaImages,
+  },
+  {
+    title: "Konsultasi",
+    description:
+      "All Mitra dapat berkonsultasi langsung ke Leader/Distributor dan pembimbing dari Pusat.",
+    Icon: FaComments,
+  },
+  {
+    title: "Visit",
+    description:
+      "Memiliki kesempatan untuk berkunjung langsung ke pabrik PT SR12 Herbal Skincare.",
+    Icon: FaBuilding,
+  },
+  {
+    title: "Katalog",
+    description: "Mendapat katalaog terupade serta produk knowlage.",
+    Icon: FaTasks,
+  },
+];
